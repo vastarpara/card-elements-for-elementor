c5f27a328d589cccccede04e30661611cd8bc136
dev9techeshta <65274595+dev9techeshta@users.noreply.github.com>
2020-05-21 15:31:06 +0530

    Update elementor-recipe-card-4.php

--- include/recipe-card/elementor-recipe-card-4.php
+++ include/recipe-card/elementor-recipe-card-4.php
@@ -1,16 +1,16 @@
 <!-- Start Recipe Card 4 -->
 <div class="recipe-card-style-4">
 	<a href="<?php echo $settings['button_link']; ?>">
-		<div class="container">
-			<div class="recipe-container" style="background-image:url(<?php echo $settings['dish_image']['url']; ?>);">
+		<div class="recipe-main-container">
+			<div class="recipe-container" style="background-image:url(<?php echo $settings['dish_image']['url']; ?>);" alt="<?php _e('Dish Image', CEE_DOMAIN) ?>" />
 				<div class="recipe-content-min bg-color">
-					<div><p class="recipe-detail-no"><?php echo $settings['recipe_minutes']; ?></p><span class="recipe-detail-text">min.</span></div>
+					<div><p class="recipe-detail-no"><?php echo $settings['recipe_minutes']; ?></p><span class="recipe-detail-text"><?php _e('min.', CEE_DOMAIN) ?></span></div>
 				</div>
 				<div class="recipe-content-ser bg-color">
-					<div><p class="recipe-detail-no"><?php echo $settings['recipe_servings']; ?></p><span class="recipe-detail-text">serv.</span></div>
+					<div><p class="recipe-detail-no"><?php echo $settings['recipe_servings']; ?></p><span class="recipe-detail-text"><?php _e('serv.', CEE_DOMAIN) ?></span></div>
 				</div>
 				<div class="recipe-content-ing bg-color">
-					<div><p class="recipe-detail-no"><?php echo $settings['recipe_ingredients']; ?></p><span class="recipe-detail-text">ing.</span></div>
+					<div><p class="recipe-detail-no"><?php echo $settings['recipe_ingredients']; ?></p><span class="recipe-detail-text"><?php _e('ing.', CEE_DOMAIN) ?></span></div>
 				</div>
 			</div>
 			<div class="recipe-container-content">
