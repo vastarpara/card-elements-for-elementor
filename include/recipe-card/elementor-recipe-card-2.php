ba75ee4df1402770965d263d810fd841d3999423
dev9techeshta <65274595+dev9techeshta@users.noreply.github.com>
2020-05-21 15:29:36 +0530

    Update elementor-recipe-card-2.php

--- include/recipe-card/elementor-recipe-card-2.php
+++ include/recipe-card/elementor-recipe-card-2.php
@@ -1,15 +1,15 @@
 <!-- Start Recipe Card 2 -->
-<div class="recipe-card-style-2" style="background-image:url(<?php echo $settings['dish_image']['url']; ?>);">
-    <div class="container">
+<div class="recipe-card-style-2" style="background-image:url(<?php echo $settings['dish_image']['url']; ?>);" >
+    <div class="recipe-main-container">
 		<div class="recipe-container">
 			<div class="recipe-content">
 				<h3 class="elementor-recipe-name-wrapper"><?php echo $settings['dish_name']; ?></h3>
 				<p class="elementor-recipe-type-wrapper"><?php echo $settings['dish_type']; ?></p>
 			</div>
 			<ul class="recipe-detail">
-				<li class="recipe-detail-list"><span class="recipe-detail-no"><?php echo $settings['recipe_minutes']; ?></span><span class="recipe-detail-text">Minutes</span></li>
-				<li class="recipe-detail-list"><span class="recipe-detail-no"><?php echo $settings['recipe_servings']; ?></span><span class="recipe-detail-text">Servings</span></li>
-				<li class="recipe-detail-list"><span class="recipe-detail-no"><?php echo $settings['recipe_ingredients']; ?></span><span class="recipe-detail-text">Ingredients</span></li>
+				<li class="recipe-detail-list"><span class="recipe-detail-no"><?php echo $settings['recipe_minutes']; ?></span><span class="recipe-detail-text"><?php _e('Minutes', CEE_DOMAIN) ?></span></li>
+				<li class="recipe-detail-list"><span class="recipe-detail-no"><?php echo $settings['recipe_servings']; ?></span><span class="recipe-detail-text"><?php _e('Servings', CEE_DOMAIN) ?></span></li>
+				<li class="recipe-detail-list"><span class="recipe-detail-no"><?php echo $settings['recipe_ingredients']; ?></span><span class="recipe-detail-text"><?php _e('Ingredients', CEE_DOMAIN) ?></span></li>
 			</ul>
 			<div class="elementor-recipe-description-wrapper">
 				<?php echo $settings['recipe_description']; ?>
