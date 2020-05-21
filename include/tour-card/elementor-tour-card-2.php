7c1182d17027f79ab2e50f2f66007c3671c04f76
dev9techeshta <65274595+dev9techeshta@users.noreply.github.com>
2020-05-21 15:33:51 +0530

    Update elementor-tour-card-2.php

--- include/tour-card/elementor-tour-card-2.php
+++ include/tour-card/elementor-tour-card-2.php
@@ -1,6 +1,6 @@
 <!-- Start Tour Card 2 -->
 <div class="tour-card-style-2" style="background-image:url(<?php echo $settings['place_image']['url']; ?>);">
-    <div class="container">
+    <div class="tour-main-container">
 		<div class="tour-container">
 			<div class="tour-content">
 				<div class="elementor-tour-name-wrapper"><?php echo $settings['place_name']; ?></div>
@@ -8,9 +8,9 @@
 			</div>
 			<div class="tour-details">
 				<ul class="tour-detail-ul">
-					<li class="tour-detail-list"><p class="tour-detail-icon"><i class="fas fa-sun"></i></p><p class="tour-detail-text"><?php echo $settings['tour_days']; ?> Days</p></li>
-					<li class="tour-detail-list"><p class="tour-detail-icon"><i class="fas fa-users"></i></p><p class="tour-detail-text"><?php echo $settings['tour_person']; ?> Persons</p></li>
-					<li class="tour-detail-list"><p class="tour-detail-icon"><i class="fas fa-flag"></i></p><p class="tour-detail-text"><?php echo $settings['tour_guides']; ?> Guides</p></li>
+					<li class="tour-detail-list"><p class="tour-detail-icon"><i class="fas fa-sun"></i></p><p class="tour-detail-text"><?php echo $settings['tour_days']; ?> <?php _e('Days', CEE_DOMAIN) ?></p></li>
+					<li class="tour-detail-list"><p class="tour-detail-icon"><i class="fas fa-users"></i></p><p class="tour-detail-text"><?php echo $settings['tour_person']; ?> <?php _e('Persons', CEE_DOMAIN) ?></p></li>
+					<li class="tour-detail-list"><p class="tour-detail-icon"><i class="fas fa-flag"></i></p><p class="tour-detail-text"><?php echo $settings['tour_guides']; ?> <?php _e('Guides', CEE_DOMAIN) ?></p></li>
 				</ul>
 			</div>
 			<div class="elementor-tour-description-wrapper">
