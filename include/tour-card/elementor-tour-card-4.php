9d6b7e34ff2cc10b3bf3e6a95eef1acb6443d414
dev9techeshta <65274595+dev9techeshta@users.noreply.github.com>
2020-05-21 15:35:09 +0530

    Update elementor-tour-card-4.php

--- include/tour-card/elementor-tour-card-4.php
+++ include/tour-card/elementor-tour-card-4.php
@@ -1,14 +1,14 @@
 <!-- Start Tour Card 4 -->
 <div class="tour-card-style-4">
 	<a href="<?php echo $settings['button_link']; ?>">
-		<div class="container">
+		<div class="tour-main-container">
 			<div class="container-inner">
 				<div class="tour-content">
 					<div class="elementor-tour-name-wrapper"><?php echo $settings['place_name']; ?></div>
 					<div class="elementor-tour-price-wrapper"><?php echo $settings['tour_price']; ?></div>
 				</div>
 				<div class="tour-image-container">
-					<img src="<?php echo $settings['place_image']['url']; ?>" class="tour-img">
+					<img src="<?php echo $settings['place_image']['url']; ?>" class="tour-img" alt="<?php _e('Tour Image', CEE_DOMAIN) ?>" />
 					<div class="tour-desc-inner">
                         <p class="elementor-tour-description-wrapper"><?php echo $settings['tour_description']; ?></p>
                     </div>
